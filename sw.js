0265c69acf82b3eef613414d465ea9c44b2a1e0a
huangnian1126-ui <huangnian1126@gmail.com>
2026-09-13 05:58:26 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
+const CACHE_NAME = 'couple-box-v1';
+
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        'index.html',
-        'manifest.json',
-        'logo.png'
+        './',
+        './index.html',
+        './manifest.json',
+        './logo.png'
       ]);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', function(e) {
